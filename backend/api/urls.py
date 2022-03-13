961e9a9b41f4650bd7aa237089aa8e3db8ff0c90
thierno <thiernoibrahimalo@esp.sn>
2022-03-13 13:08:01 +0000

    adding generic API vew #234

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,12 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('', views.api_over_view, name='api_over_view'),
-    path('task-list', views.task_list, name='task_list'),
-    path('task-detail/<int:pk>', views.task_detail, name='task-detail'),
-    path('task-create', views.task_create, name='task-create'),
-    path('task-update/<int:pk>', views.task_update, name='task-update'),
-    path('task-delete/<int:pk>', views.task_delete, name='task-delete'),
+    # path('', views.api_over_view, name='api_over_view'),
+    path('', views.TodoMixinView.as_view(), name='task_list'),
+    path('<int:pk>/', views.TodoMixinView.as_view(), name='task-detail'),
+
 
 
 
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,49 +1,106 @@
 from asyncio import Task
 from django.shortcuts import render
 from rest_framework.decorators import api_view
+from rest_framework import generics,mixins
 from rest_framework.response import Response
 from .serializers import TaskSerializer
 from .models import Task
 # Create your views here.
-@api_view(['GET'])
-def api_over_view(request):
-    return Response("hello world")
+# @api_view(['GET'])
+# def api_over_view(request):
+#     return Response("hello world")
 
-@api_view(['GET'])
-def task_list(request):
-    tasks=Task.objects.all()
-    serializer=TaskSerializer(tasks, many=True)
-    return Response(serializer.data)
+# @api_view(['GET'])
+# def task_list(request):
+#     tasks=Task.objects.all()
+#     serializer=TaskSerializer(tasks, many=True)
+#     return Response(serializer.data)
 
-@api_view(['GET'])
-def task_detail(request, pk: int):
-    tasks=Task.objects.get(id=(pk))
-    serializer=TaskSerializer(tasks, many=False)
-    return Response(serializer.data)
+# @api_view(['GET'])
+# def task_detail(request, pk: int):
+#     tasks=Task.objects.get(id=(pk))
+#     serializer=TaskSerializer(tasks, many=False)
+#     return Response(serializer.data)
 
-@api_view(['POST'])
-def task_create(request):
-    serializer=TaskSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
+# @api_view(['POST'])
+# def task_create(request):
+#     serializer=TaskSerializer(data=request.data)
+#     if serializer.is_valid():
+#         serializer.save()
         
-    return Response(serializer.data)
+#     return Response(serializer.data)
 
-@api_view(['POST'])
-def task_update(request, pk: int):
-    task=Task.objects.get(id=(pk))
-    serializer=TaskSerializer(instance=task, data=request.data)
-    if serializer.is_valid():
-        serializer.save()
+# @api_view(['POST'])
+# def task_update(request, pk: int):
+#     task=Task.objects.get(id=(pk))
+#     serializer=TaskSerializer(instance=task, data=request.data)
+#     if serializer.is_valid():
+#         serializer.save()
         
-    return Response(serializer.data)
+#     return Response(serializer.data)
 
-@api_view(['DELETE'])
+# @api_view(['DELETE'])
 
-def task_delete(request, pk: int):
-    task=Task.objects.get(id=(pk))
-    task.delete()
-    return Response("Item deleted successfully")
+# def task_delete(request, pk: int):
+#     task=Task.objects.get(id=(pk))
+#     task.delete()
+#     return Response("Item deleted successfully")
 
         
-        
+# class  TodoCreate(generics.CreateAPIView):
+#     queryset = Task.objects.all()
+#     serializer_class = TaskSerializer
+    
+class  TodoDetail(generics.RetrieveAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    
+todo_detail_view=TodoDetail.as_view()
+
+class TodoListCreateView(generics.ListCreateAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+# class TodoDelete(generics.DeleteAPIView):
+#     queryset = Task.objects.all()
+#     serializer_class = TaskSerializer
+    
+
+class TodoUpdateView(generics.UpdateAPIView):
+    queryset=Task.objects.all()
+    serializer_class = TaskSerializer
+    lookup_field='pk'
+    
+class TodoDeleteView(generics.DestroyAPIView):
+    queryset=Task.objects.all()
+    serializer_class = TaskSerializer
+    lookup_field='pk'
+    
+    def perform_destroy(self, instance):
+        return super().perform_destroy(instance)
+    
+class TodoMixinView(
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.CreateModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    generics.GenericAPIView
+    ,):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    lookup_field='pk'
+    def get(self, request, *args, **kwargs):
+        pk=kwargs.get('pk')
+        if pk is not None:
+            return self.retrieve(request, *args, **kwargs)
+        return self.list(request, *args, **kwargs)
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+    pass
